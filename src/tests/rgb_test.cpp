b1fb42850139ac3352af110372c2ef2181be04a2
adrian-mosor <mosoradrian123@gmail.com>
2023-04-04 17:30:28 +0300

    Added brightness test-case

--- src/tests/rgb_test.cpp
+++ src/tests/rgb_test.cpp
@@ -5,36 +5,64 @@
 #include "messages.h"
 
 #define LED_PIN 9
-#define BRIGHTNESS 5
+#define BRIGHTNESS 1
 #define NUM_LEDS 256
 
 CRGB leds[NUM_LEDS];
 bool hi_flag = false;
 
-void setup() {
-  // put your setup code here, to run once:
+int bright = 0;
 
-  Serial.begin(9600);
-
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(BRIGHTNESS);
-
-  FastLED.clear();  //update the state of the matrix
-  FastLED.show();
-
-  print_hi();
+void increment_brightness()
+{
+    bright += 1;
 }
 
-void loop(){
-
-    // FastLED.setBrightness(BRIGHTNESS);
-    // leds[240] = CRGB::SkyBlue;
-    // FastLED.show();
+void setup()
+{
+    // put your setup code here, to run once:
     
-    // delay(2000);
+    Serial.println("RGB_TEST");
+    Serial.begin(9600);
 
+    Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    Serial.print("Setting brightness to: ");
+    Serial.println(bright);
+    Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+    FastLED.setBrightness(BRIGHTNESS);
+
+    FastLED.clear(); // update the state of the matrix
+    FastLED.show();
+
+    print_hi();
+}
+
+void loop()
+{
+
+    // test-cases used for brightness treshold values:
+    if(hi_flag == false){
+
+        for (int i = 0; i < 10; i++)    //max value = increment * steps
+        {
+            delay(2000);
+            increment_brightness();
+            Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Serial.print("Setting brightness to: ");
+            Serial.println(bright);
+            Serial.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            FastLED.setBrightness(bright);
+            FastLED.show();
+        }
+
+        hi_flag = true;
+    }
+
+    // delay(2000);
     // FastLED.setBrightness(250);
     // FastLED.show();
-    
+
     // delay(2000);
 }
